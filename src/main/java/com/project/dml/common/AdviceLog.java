ce4488e51501fdd1e80bb53f3187f35f51510c56
codefatal <eziofatal@gmail.com>
2023-08-28 15:46:31 +0900

    DML 환경구축
    pom.xml 수정
    web.xml 수정
    root-context.xml 수정
    servlet-context.xml 수정
    mybatis-config.xml 생성
    mappers 생성
    log4j.xml 수정
    log4jdbc.log4j2.properties 생성

---
+++ src/main/java/com/project/dml/common/AdviceLog.java
@@ -0,0 +1,77 @@
+package com.project.dml.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class AdviceLog {
+	private static final Logger logger = LoggerFactory.getLogger(AdviceLog.class);
+
+	//@Pointcut("execution(타겟메소드)")
+	// .. 매개변수 없거나 여러개 있거나..
+	// * 1개 이상일때 씀.
+	@Pointcut("execution(public * kh.lclass.db1..*DAO.*(..))")
+	public void daoPointCut() {}
+	
+	@Pointcut("execution(public * kh.lclass.db1..*Service.*(..))")
+	public void servicePointCut() {}
+	
+	@Pointcut("execution(public * kh.lclass.db1..*Controller.*(..))")
+	public void controllerPointCut() {}
+	
+	@Around("daoPointCut()")
+	public Object aroundDaoLog(ProceedingJoinPoint pjp) throws Throwable {
+		logger.debug("▷▷▷["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		
+		// 타겟메소드로 전달되는 매개인자들
+		Object[] args = pjp.getArgs();
+		for(int i=0; i<args.length; i++) {
+			logger.debug("▷▷▷-args["+i+"] "+args[i]+"");
+		}
+		Object robj = null; // 타겟메소드로부터 return 값을 받아 저장할 공간
+		StopWatch stopwatch = new StopWatch();
+		stopwatch.start();
+		robj = pjp.proceed();  // 타겟메소드 실행 !!! Around 중요메소드
+		stopwatch.stop();
+		logger.debug("▷▷▷[Dao ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+robj);
+		return robj; // 타겟메소드를 호출한 메소드에 return함
+	}
+	@Around("servicePointCut()")
+	public Object aroundSrvLog(ProceedingJoinPoint pjp) throws Throwable {
+		logger.debug("▷▷["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		// 타겟메소드로 전달되는 매개인자들
+		Object[] args = pjp.getArgs();
+		for(int i=0; i<args.length; i++) {
+			logger.debug("▷▷-args["+i+"] "+args[i]+"");
+		}
+		Object robj = null; // 타겟메소드로부터 return 값을 받아 저장할 공간
+		robj = pjp.proceed();  // 타겟메소드 실행 !!! Around 중요메소드
+		logger.debug("▷▷[Srv ▷]"+robj);
+		return robj; // 타겟메소드를 호출한 메소드에 return함
+	}
+	@Around("controllerPointCut()")
+	public Object aroundCtrLog(ProceedingJoinPoint pjp) throws Throwable {
+		Object robj = null; // 타겟메소드로부터 return 값을 받아 저장할 공간
+		
+		logger.debug("▷["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		// 타겟메소드로 전달되는 매개인자들
+		Object[] args = pjp.getArgs();
+		for(int i=0; i<args.length; i++) {
+			logger.debug("▷-args["+i+"] "+args[i]+"");
+		}
+		StopWatch stopwatch = new StopWatch();
+		stopwatch.start();
+		robj = pjp.proceed();  // 타겟메소드 실행 !!! Around 중요메소드
+		stopwatch.stop();
+		logger.debug("▷[Ctr ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+robj);
+		return robj; // 타겟메소드를 호출한 메소드에 return함
+	}
+	
+}
