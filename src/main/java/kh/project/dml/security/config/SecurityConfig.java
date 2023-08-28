4870f070214ff9ee5f3145f9a444832f7e0de3f5
codefatal <eziofatal@gmail.com>
2023-08-28 16:46:14 +0900

    기본틀 완성
    kh.properties -> dml.properties 이름 변경
    기본 보안 설정 파일 세팅

--- src/main/java/kh/project/dml/security/config/SecurityConfig.java
+++ src/main/java/kh/project/dml/security/config/SecurityConfig.java
@@ -20,24 +20,24 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests() // 로그인없이 접근 가능한 경로
-//                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll();
 //                		.antMatchers("/", "/crytochart", "/news", "/crytomarket", "/user/login", "/user/signup", "/css/**", "/fontawesome/**", "/fonts/**", "/img/**", "/js/**", "bootstrap.min.js").permitAll()
 //                		.anyRequest().authenticated()
-                .and()
-                .formLogin() // 로그인 페이지(위에 접근 불가한 경로 접근 시 자동 이동)
+//                .and()
+//                .formLogin() // 로그인 페이지(위에 접근 불가한 경로 접근 시 자동 이동)
 //                        .loginPage("/user/login")
-                        .defaultSuccessUrl("/")
-                .and()
-                .logout() // 로그아웃
+//                        .defaultSuccessUrl("/")
+//                .and()
+//                .logout() // 로그아웃
 //                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                .and()
-                .csrf() // csrf 취약점으로 접근 제한된 페이지 허용
+//                        .logoutSuccessUrl("/")
+//                        .invalidateHttpSession(true)
+//                .and()
+//                .csrf() // csrf 취약점으로 접근 제한된 페이지 허용
 //		                .ignoringAntMatchers("/coin/prices/buy", "/coin/prices/sell")
-                .and()
-                .sessionManagement() // 세션 유효 유무
-                	.invalidSessionUrl("/"); // 세션이 유효하지 않을 때 메인 페이지로 리다이렉트
+//                .and()
+//                .sessionManagement() // 세션 유효 유무
+//                	.invalidSessionUrl("/"); // 세션이 유효하지 않을 때 메인 페이지로 리다이렉트
         return http.build();
     }
 
