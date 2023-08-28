62f13c736fead52af5c23a424e630d0f3ad0688b
codefatal <eziofatal@gmail.com>
2023-08-28 17:53:26 +0900

    ModelAndView 예시 작성
    ModelAndView 예시 작성

--- src/main/java/kh/project/dml/diet/model/controller/FpDietController.java
+++ src/main/java/kh/project/dml/diet/model/controller/FpDietController.java
@@ -1,8 +1,28 @@
 package kh.project.dml.diet.model.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
+import kh.project.dml.diet.model.service.FpDietService;
 
 @Controller
 public class FpDietController {
 	
+	@Autowired
+	public FpDietService fpDietService;
+	
+	// 기존 model 사용
+	public String selectList(Model model) {
+		model.addAttribute("dietList", fpDietService.selectList());
+		return "diet/list";
+	}
+	
+	// ModelAndView 사용
+	public ModelAndView selectList(ModelAndView mv) {
+		mv.addObject("dietList", fpDietService.selectList());
+		mv.setViewName("diet/list");
+		return mv;
+	}
 }
