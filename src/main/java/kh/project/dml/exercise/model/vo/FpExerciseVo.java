44884cd8aa63e5d8c8bf3dbc58fa4b8821d1a7c5
cheongha12 <cheong.9396@gmail.com>
2023-08-31 16:16:40 +0900

    FpExerciseVo 수정(exName)

--- src/main/java/kh/project/dml/exercise/model/vo/FpExerciseVo.java
+++ src/main/java/kh/project/dml/exercise/model/vo/FpExerciseVo.java
@@ -16,7 +16,7 @@ import lombok.ToString;
 public class FpExerciseVo {
 
 	private int ecode;
-	private String exname;
+	private String exName;
 	private String part;
 	private String epose;
 	private String eposeLink;
