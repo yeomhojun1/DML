91af7ab4772a5ae6f004be0b854b11ee89657281
Sonbeomgyu1 <sjk6285@naver.com>
2023-09-18 11:14:46 +0900

    캘린더 팝업창.memberId값 가져오기 구현완료, 제품추천 (닭가슴살 미구현)
    .

--- src/main/java/kh/project/dml/calender/controller/FpCalenderController.java
+++ src/main/java/kh/project/dml/calender/controller/FpCalenderController.java
@@ -1,30 +1,61 @@
 package kh.project.dml.calender.controller;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import kh.project.dml.calender.model.service.FpCalenderServiceImpl;
+import kh.project.dml.calender.model.service.FpCalenderService;
+import kh.project.dml.calender.model.vo.FpCalenderVo;
+import kh.project.dml.common.interceptor.SessionNames;
+import kh.project.dml.member.model.vo.FpMemberVo;
 
 @Controller
 @RequestMapping("/calendar")
 public class FpCalenderController {
 
 	@Autowired
-	FpCalenderServiceImpl fpCalenderServiceImpl;
+	FpCalenderService fpCalenderServiceImpl;
+	
+//	@Autowired
+//	SessionUtil sessionUtil;
 	
 	@RequestMapping //기본 페이지 표시
 	public String viewCalendar(Model model, HttpSession session){
+		
+		// 로그인 정보 session
+		FpMemberVo loginInfo = (FpMemberVo) session.getAttribute(SessionNames.LOGIN);
+		// sessionUtil.getMemberId();
+		
+		List<FpCalenderVo> list = fpCalenderServiceImpl.selectList(loginInfo);
+
+		model.addAttribute("getCalendarList", list);
 		return "/fullcalendar/calendar";
 	}
+	
+	//클릭된날짜에 대한 일정 추가 팝업 show
+	@GetMapping("/calendarSelected")
+//	@RequestMapping(value = "/calendarSeleted", method = RequestMethod.GET)
+	public String calendarSeleted(@RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+									@RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate, Model model) {
+		
+		model.addAttribute("startDate", startDate);
+		model.addAttribute("endDate", endDate);
+		
+		return "/fullcalendar/addcalendar"; 
+	}
+	 
 	
 
 
---
+++ src/main/webapp/WEB-INF/views/fullcalendar/addcalendar.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+   
+   <script src='<c:url value="/resources/js/custom-calendar.js" />'></script>
+<title>Insert title here</title>
+</head>
+<body style="text-align: center">
+<div>
+	<h1>일정 추가</h1>
+</div>
+<div>
+	<from id="calendarData">
+	<div>
+		제목 : <input type="text" name="title" id="title" placeholder="제목 입력">
+	</div>
+	<br>
+	<div>
+		시작 날짜 : <input type="text" name="startdate" id="startdate" size="12" value="${startDate}"/>
+				 <input type="button" value="달력" onclick="${'#startdate'}.datepicker('show');"/>
+	</div>
+	<br>
+	<div>
+		종료 날짜 : <input type="text" name="enddate" id="enddate" size="12" value="${endDate}"/>
+				  <input type="button" value="달력" onclick="${'#enddate'}.datepicker('show');"/>
+	</div>
+	<br>
+		설명
+	<div>
+			<textarea name="content" id="content" rows="10" cols="30"></textarea>
+	</div>
+	<br>
+	</from>
+			<input type = "button" value="저장" onclick="send_save()"/>
+</div>
+
+</body>
+</html>
