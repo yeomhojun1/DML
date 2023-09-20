74c6a67dc7035857ac25b0a314e82f53c5949db2
Sonbeomgyu1 <sjk6285@naver.com>
2023-09-20 10:26:19 +0900

    제품추천 (dao vo controller service ) 완료.
    .

--- src/main/java/kh/project/dml/nutrient/controller/FpNntruentController.java
+++ src/main/java/kh/project/dml/nutrient/controller/FpNntruentController.java
@@ -1,5 +1,103 @@
 package kh.project.dml.nutrient.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kh.project.dml.daydiet.model.vo.FpDayDietVo;
+import kh.project.dml.nutrient.model.service.FpNntruentService;
+import kh.project.dml.nutrient.model.vo.FpNntruentVo;
+
+@Controller
+@RequestMapping("/nutruent")
 public class FpNntruentController {
 
+	@Autowired
+	private FpNntruentService fpNntruentServiceImpl;
+	
+	@GetMapping("/list")
+	public ModelAndView selectList(ModelAndView mv) {
+		mv.addObject("nutruentlist", fpNntruentServiceImpl.selectList());
+		mv.setViewName("nutruent/list");
+		return mv;
+	}
+	
+	@GetMapping("/one")
+	public ModelAndView selectOnenutruent(ModelAndView mv, int nutrientId) {
+		mv.addObject("nutruentone", fpNntruentServiceImpl.selectOne(nutrientId));
+		mv.setViewName("nutruent/one");
+		return mv;
+	}
+	
+	@GetMapping("/insert")
+	public ModelAndView insertnutruent(ModelAndView mv ) {
+		mv.setViewName("nutruent/insert");
+		return mv;
+	}
+	
+	@PostMapping("/insert")
+	public String insertDoMemeber(RedirectAttributes redirectAttr, FpNntruentVo vo) {
+		String viewPage = "redirect:/";
+		int result = fpNntruentServiceImpl.insert(vo);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 가입 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/nutruent/insert";
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 가입 됐습니다");
+			viewPage = "redirect:/nutruent/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
+	@GetMapping("/update")
+	public ModelAndView updatedaydiet(ModelAndView mv, int nutrientId ) {
+		mv.addObject("nutruentone", fpNntruentServiceImpl.selectOne(nutrientId));
+		mv.setViewName("nutruent/update");
+		return mv;
+	}
+	
+	@PostMapping("/update")
+	public String updateDoMemeber(RedirectAttributes redirectAttr, FpNntruentVo vo ) {
+		String viewPage = "redirect:/";
+		int result = fpNntruentServiceImpl.update(vo);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 수정 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/nutruent/update";
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 수정 됐습니다");
+				viewPage = "redirect:/nutruent/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
+	@PostMapping("/delete")
+	public String deleteDoMemeber(RedirectAttributes redirectAttr,int nutrientId ) {
+		String viewPage = "redirect:/";
+		int result = fpNntruentServiceImpl.delete(nutrientId);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 삭제 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/nutruent/list";//delete는 보통 처음에 있던 화면으로 돌아감 그래서 ajax를 쓰는데 그건 추후
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 삭제 됐습니다");
+				viewPage = "redirect:/nutruent/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
 }
--- src/main/java/kh/project/dml/producttype/controller/FpProducttypeController.java
+++ src/main/java/kh/project/dml/producttype/controller/FpProducttypeController.java
@@ -1,5 +1,101 @@
 package kh.project.dml.producttype.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kh.project.dml.daydiet.model.vo.FpDayDietVo;
+import kh.project.dml.producttype.model.service.FpProducttypeService;
+import kh.project.dml.producttype.model.vo.FpProducttypeVo;
+
+@Controller
+@RequestMapping("/producttype")
 public class FpProducttypeController {
 
+	@Autowired
+	private FpProducttypeService fpProducttypeServiceImpl;
+	
+	@GetMapping("/list")
+	public ModelAndView selectList(ModelAndView mv) {
+		mv.addObject("producttypelist", fpProducttypeServiceImpl.selectList());
+		mv.setViewName("producttype/list");
+		return mv;
+	}
+	
+	@GetMapping("/one")
+	public ModelAndView selectOnedaydiet(ModelAndView mv, String type) {
+		mv.addObject("producttypeone", fpProducttypeServiceImpl.selectOne(type));
+		mv.setViewName("producttype/one");
+		return mv;
+	}
+	@GetMapping("/insert")
+	public ModelAndView insertdaydiet(ModelAndView mv ) {
+		mv.setViewName("producttype/insert");
+		return mv;
+	}
+	@PostMapping("/insert")
+	public String insertDoMemeber(RedirectAttributes redirectAttr, FpProducttypeVo vo) {
+		String viewPage = "redirect:/";
+		int result = fpProducttypeServiceImpl.insert(vo);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 가입 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/producttype/insert";
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 가입 됐습니다");
+				viewPage = "redirect:/producttype/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
+	@GetMapping("/update")
+	public ModelAndView updatedaydiet(ModelAndView mv, String type ) {
+		mv.addObject("producttypeone", fpProducttypeServiceImpl.selectOne(type));
+		mv.setViewName("producttype/update");
+		return mv;
+	}
+	
+	@PostMapping("/update")
+	public String updateDoMemeber(RedirectAttributes redirectAttr, FpProducttypeVo vo ) {
+		String viewPage = "redirect:/";
+		int result = fpProducttypeServiceImpl.update(vo);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 수정 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/producttype/update";
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 수정 됐습니다");
+				viewPage = "redirect:/producttype/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
+	@PostMapping("/delete")
+	public String deleteDoMemeber(RedirectAttributes redirectAttr,String type ) {
+		String viewPage = "redirect:/";
+		int result = fpProducttypeServiceImpl.delete(type);
+		try {
+			if (result < 1) {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 삭제 실패했습니다 \n 다시 입력해주세요");
+				viewPage = "redirect:/producttype/list";//delete는 보통 처음에 있던 화면으로 돌아감 그래서 ajax를 쓰는데 그건 추후
+			} else {
+				redirectAttr.addFlashAttribute("msg", "회원 정보 삭제 됐습니다");
+				viewPage = "redirect:/producttype/list";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return viewPage;
+	}
+	
 }
